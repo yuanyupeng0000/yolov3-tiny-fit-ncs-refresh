d5fe2bde4ff1c946733a6f450bd17640bc370640
yuanyupeng0000 <yuanyupeng-1989@163.com>
2020-05-20 14:04:44 +0800

    rm zen_rtsp

--- zen_rtsp/alg-mutilthread/algorithm.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ALGORITH_H_H__
-#define __ALGORITH_H_H__
-
-int InitAlgorithm(CPoint m_ptEnd[]);
-int AlgProcessFrameData(int index, unsigned char *inbuf, unsigned char *ubuf,unsigned char *vbuf,int width, int height);
-
-#endif
--- zen_rtsp/res/readme.txt
+++
@@ -1,2 +0,0 @@
-config.json:main config file, client handle this.
-server.json:server global config when server startup, we can modify it manually
